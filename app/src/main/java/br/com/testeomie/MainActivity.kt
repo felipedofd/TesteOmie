ad6491409d7321e251d2a09cd7b6689020c0ae1d
Felipe Anzolin <75090553+felipedofd@users.noreply.github.com>
2024-11-02 02:30:54 -0300

    Update MainActivity.kt
    Create a entryPoint view in mainActivity
    Setup TopAppBar
    Setup SplashScreen

--- app/src/main/java/br/com/testeomie/MainActivity.kt
+++ app/src/main/java/br/com/testeomie/MainActivity.kt
@@ -1,47 +1,70 @@
 package br.com.testeomie
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import br.com.testeomie.presentation.HomeScreen
 import br.com.testeomie.ui.theme.TesteOmieTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { true }
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000L)
+            splashScreen.setKeepOnScreenCondition { false }
+        }
         setContent {
             TesteOmieTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                    TopAppBar(title = { title }, navigationIcon = {
+                        IconButton(onClick = {}, modifier = Modifier.fillMaxSize()) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_omie_text),
+                                contentDescription = "",
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }, colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = colorResource(id = R.color.omie_color)
                     )
+                    )
+                }) {
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainActivityPreview() {
     TesteOmieTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
