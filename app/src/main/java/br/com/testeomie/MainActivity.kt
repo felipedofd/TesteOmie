67dcd4e28e84451e0f45d7cd6a0a26d1dcec8dbd
Felipe Anzolin <75090553+felipedofd@users.noreply.github.com>
2024-11-04 09:03:35 -0300

    Update MainActivity.kt

--- app/src/main/java/br/com/testeomie/MainActivity.kt
+++ app/src/main/java/br/com/testeomie/MainActivity.kt
@@ -5,18 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import br.com.testeomie.presentation.HomeScreen
@@ -27,7 +16,6 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,24 +27,7 @@ class MainActivity : ComponentActivity() {
             splashScreen.setKeepOnScreenCondition { false }
         }
         setContent {
-            TesteOmieTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-                    TopAppBar(title = { title }, navigationIcon = {
-                        IconButton(onClick = {}, modifier = Modifier.fillMaxSize()) {
-                            Image(
-                                painter = painterResource(id = R.drawable.ic_omie_text),
-                                contentDescription = "",
-                                modifier = Modifier.fillMaxWidth()
-                            )
-                        }
-                    }, colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = colorResource(id = R.color.omie_color)
-                    )
-                    )
-                }) {
-                    HomeScreen()
-                }
-            }
+            HomeScreen()
         }
     }
 }
