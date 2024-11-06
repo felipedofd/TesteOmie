8876e43023f1603e046c4d45578747dd192eb494
Felipe Anzolin <75090553+felipedofd@users.noreply.github.com>
2024-11-05 21:46:37 -0300

    Delete Sales.kt

--- app/src/main/java/br/com/testeomie/model/Sales.kt
+++
@@ -1,8 +0,0 @@
-package br.com.testeomie.model
-
-data class Sales(
-    val clientName: String? = null,
-    val productDescription: String? = null,
-    val productQuantity: String? = null,
-    val unitaryProductValue: String? = null,
-)
