38ecd71eb7b59871caddd45dbe6014f03cec2e99
Felipe Anzolin <felipedofd@hotmail.com>
2024-11-06 05:25:14 -0300

    Update TotalSellingValueComponent.kt

--- app/src/main/java/br/com/testeomie/presentation/components/TotalSellingValueComponent.kt
+++ app/src/main/java/br/com/testeomie/presentation/components/TotalSellingValueComponent.kt
@@ -4,25 +4,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import br.com.testeomie.R
 
 @Composable
 fun TotalSellingValueComponent(totalSellingValue: Double, modifier: Modifier) {
     Text(
-        text = "Total de vendas: R$$totalSellingValue",
-        fontSize = 30.sp ,
+        text = stringResource(R.string.total_sales_value_text, totalSellingValue),
+        fontSize = 30.sp,
         color = Color.DarkGray,
         fontWeight = FontWeight.Bold,
         textAlign = TextAlign.Left
-
     )
-}
-
-@Preview
-@Composable
-fun TotalSellingValueComponentPreview() {
-    TotalSellingValueComponent(totalSellingValue = 27.45  , modifier = Modifier)
 }
