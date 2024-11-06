dc5a8db79f2a67206477450fb6edfbb92c8d47fe
Felipe Anzolin <75090553+felipedofd@users.noreply.github.com>
2024-11-05 21:46:45 -0300

    Update MainActivity.kt

--- app/src/main/java/br/com/testeomie/MainActivity.kt
+++ app/src/main/java/br/com/testeomie/MainActivity.kt
@@ -1,33 +1,44 @@
 package br.com.testeomie
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModelProvider
 import br.com.testeomie.presentation.HomeScreen
+import br.com.testeomie.presentation.HomeScreenViewModel
 import br.com.testeomie.ui.theme.TesteOmieTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val homeScreenViewModel: HomeScreenViewModel by lazy {
+        ViewModelProvider(this).get(HomeScreenViewModel::class.java)
+    }
+
+    private val clientLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                homeScreenViewModel.onLoadSales()
+            }
+        }
+
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         enableEdgeToEdge()
-        val splashScreen = installSplashScreen()
-        splashScreen.setKeepOnScreenCondition { true }
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(3000L)
-            splashScreen.setKeepOnScreenCondition { false }
-        }
+        installSplashScreen()
         setContent {
-            HomeScreen()
+            HomeScreen(homeScreenViewModel, onAddClientClick = {
+                clientLauncher.launch(Intent(this, ClientActivity::class.java))
+            })
         }
     }
 }
@@ -36,6 +47,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainActivityPreview() {
     TesteOmieTheme {
-        HomeScreen()
+//        HomeScreen()
     }
 }
