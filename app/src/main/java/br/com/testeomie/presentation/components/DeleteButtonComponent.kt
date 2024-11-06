0c3726b6d8d2cf873dfd72e381116c13f4892084
Felipe Anzolin <felipedofd@hotmail.com>
2024-11-06 05:25:23 -0300

    Update DeleteButtonComponent.kt

--- app/src/main/java/br/com/testeomie/presentation/components/DeleteButtonComponent.kt
+++ app/src/main/java/br/com/testeomie/presentation/components/DeleteButtonComponent.kt
@@ -8,8 +8,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import br.com.testeomie.R
 
 @Composable
 fun DeleteButtonComponent(onClick: () -> Unit, modifier: Modifier) {
@@ -21,13 +22,7 @@ fun DeleteButtonComponent(onClick: () -> Unit, modifier: Modifier) {
     ) {
         Icon(
             imageVector = Icons.Filled.Delete,
-            contentDescription = "Exclude Button",
+            contentDescription = stringResource(R.string.exclude_button_content_description),
         )
     }
-}
-
-@Composable
-@Preview
-fun DeleteButtonComponentPreview() {
-    DeleteButtonComponent({}, modifier = Modifier)
 }
