285356f075ae8a5c37ca817bff3f3b9a7a3cfe5c
Felipe Anzolin <felipedofd@hotmail.com>
2024-11-06 05:25:18 -0300

    Update SelesListItem.kt

--- app/src/main/java/br/com/testeomie/presentation/components/item/SelesListItem.kt
+++ app/src/main/java/br/com/testeomie/presentation/components/item/SelesListItem.kt
@@ -16,15 +16,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import br.com.testeomie.R
 import br.com.testeomie.model.Sale
 import br.com.testeomie.presentation.components.DeleteButtonComponent
 
 @Composable
 fun SalesListItem(
-    item: Sale,
-    onConfirmDelete: (uuid: String?) -> Unit = {}
+    item: Sale, onConfirmDelete: (uuid: String?) -> Unit = {}
 ) {
 
     val context = LocalContext.current
@@ -33,12 +33,12 @@ fun SalesListItem(
         onPositive: () -> Unit
     ) {
         val alertDialog = AlertDialog.Builder(context)
-        alertDialog.setTitle("Deletar venda").setMessage("Deseja excluir a venda?")
-            .setPositiveButton("Sim") { _, _ ->
+        alertDialog.setTitle(context.getString(R.string.delete_sales_text))
+            .setMessage(context.getString(R.string.exclude_sales_confirmation_text))
+            .setPositiveButton(context.getString(R.string.alert_dialog_button_positive_text)) { _, _ ->
                 onPositive()
-
-            }.setNegativeButton("Não") { _, _ ->
-
+            }
+            .setNegativeButton(context.getString(R.string.alert_dialog_negative_button_text)) { _, _ ->
             }.show()
     }
 
@@ -55,18 +55,14 @@ fun SalesListItem(
                 .fillMaxWidth(),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
+            Spacer(modifier = Modifier.height(2.dp))
             Text(
-                text = "UUID: ${item.uuid}",
+                text = stringResource(R.string.client_name_text, item.name.toString()),
                 style = MaterialTheme.typography.bodyMedium
             )
             Spacer(modifier = Modifier.height(2.dp))
             Text(
-                text = "Cliente: ${item.name}",
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Spacer(modifier = Modifier.height(2.dp))
-            Text(
-                text = "Produtos:",
+                text = stringResource(R.string.products_text),
                 style = MaterialTheme.typography.bodyMedium
             )
             item.products?.forEach { product ->
@@ -79,18 +75,6 @@ fun SalesListItem(
                     },
                 )
             }, Modifier.align(Alignment.End))
-
         }
     }
-}
-
-@Composable
-@Preview
-fun SalesListItemPreview() {
-//    SalesListItem(
-//        Product(
-//            "Bolo", "um", "200",
-//        )
-//    )
-
 }
